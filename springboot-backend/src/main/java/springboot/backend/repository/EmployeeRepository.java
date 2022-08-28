1944244d32a03c948f786863d147339dc73e0815
Kiran Shiragumpi <kshiragumpi@gmail.com>
2022-08-28 11:40:22 +0530

    new employee project

---
+++ springboot-backend/src/main/java/springboot/backend/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package springboot.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import springboot.backend.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
